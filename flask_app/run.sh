5e683b6803ee2885c295934190ff255994e58d4c
T1rax <nikishov.v@mail.ru>
2023-04-04 11:59:51 +0400

    First files

---
+++ flask_app/run.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+set -e
+
+#gunicorn main:app --bind 0.0.0.0:8000 --workers 3 --worker-class uvicorn.workers.UvicornWorker --log-file=- --access-logfile=- --error-logfile=-
+
+python3 wsgi_app.py
