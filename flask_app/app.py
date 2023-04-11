d7e70e03d8d49d41233ac752036d13de0a863494
mod-web <module-web@yandex.ru>
2023-04-11 16:58:38 +0300

    !add models in app.py

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,4 +1,8 @@
+import enum
 import sys
+
+from sqlalchemy import Enum
+
 sys.path.insert(0, '/home/tirax/movies_auth_service')
 
 from flask import Flask
@@ -30,18 +34,49 @@ db = SQLAlchemy(app)
 jwt_redis_blocklist = redis.Redis(host=configs.mds.host, port=configs.mds.port, db=0, decode_responses=True)
 jwt = JWTManager(app)
 
+# Models
 class User(db.Model):
+    """ Model for User """
     __tablename__ = 'users'
 
-    id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
-    login = db.Column(db.String, unique=True, nullable=False)
+    id = db.Column(UUID(as_uuid=True),
+                   primary_key=True,
+                   default=uuid.uuid4,
+                   unique=True,
+                   nullable=False)
+    login = db.Column(db.String(100), unique=True, nullable=False)
+    # email = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String, nullable=False)
-    first_name = db.Column(db.String, nullable=False)
-    last_name = db.Column(db.String, nullable=False)
+    first_name = db.Column(db.String(100), nullable=False)
+    last_name = db.Column(db.String(100), nullable=False)
     roles = db.Column(db.PickleType, nullable=False)
 
     def __repr__(self):
-        return f'<User {self.login}>' 
+        return f'<User {self.login}>'
+
+
+class ActionType(enum.Enum):
+    """ Action types for UserHistory """
+    login = 'login'
+    logout = 'logout'
+
+
+class UserHistory(db.Model):
+    """ Model for recording user login history """
+    __tablename__ = 'histories'
+
+    id = db.Column(UUID(as_uuid=True),
+                   primary_key=True,
+                   default=uuid.uuid4,
+                   unique=True,
+                   nullable=False)
+    useragent = db.Column(db.String(100), nullable=False)
+    action = db.Column(Enum(ActionType))
+    timestamp = db.Column(db.DateTime, default=datetime.now())
+    user_id = db.Column(UUID(as_uuid=True), db.ForeignKey('users.id'))
+
+    def __repr__(self):
+        return f'<UserHistory {self.user_id}>'
 
 
 with app.app_context():
